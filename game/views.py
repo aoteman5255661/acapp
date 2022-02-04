2de4baa864f94a6773795111302699eccb18c1d2
Your Name <394156907@qq.com>
2022-02-04 19:54:06 +0800

    1

--- game/views.py
+++ game/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    line1 = '<h1 style="text-align:center">我的第一个网页</h1>'
+    line2 = '<img src="https://i1.hdslb.com/bfs/archive/ba81e7381ce454263069e2f0be12c740a9df2994.jpg@672w_378h_1c.webp" width=2000>'
+    return HttpResponse(line1+line2)
 
 
