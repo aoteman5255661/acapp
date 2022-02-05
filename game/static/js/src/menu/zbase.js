991e02ec2faef909b6bcae049201b3cb628945c8
Your Name <394156907@qq.com>
2022-02-05 16:42:14 +0800

    1

--- game/static/js/src/menu/zbase.js
+++ game/static/js/src/menu/zbase.js
@@ -5,15 +5,15 @@ class AcGameMenu{
         this.$menu = $(`
 <div class="ac-game-menu">
     <div class="ac-game-menu-field">
-        <div class="ac-game-menu-field-item ac-game-menu-field-single" >
+        <div class="ac-game-menu-field-item ac-game-menu-field-single-mode" >
             单人模式
         </div>
         <br>
-        <div class="ac-game-menu-field-item ac-game-menu-field-multi">
+        <div class="ac-game-menu-field-item ac-game-menu-field-multi-mode">
             多人模式
         </div>
         <br>
-        <div class="ac-game-menu-field-item ac-game-menu-field-item-settings">
+        <div class="ac-game-menu-field-item ac-game-menu-field-settings">
             设置
         </div>
     </div>
@@ -21,8 +21,29 @@ class AcGameMenu{
         `);
         console.log(3333)
         this.root.$ac_game.append(this.$menu);
-        this.$single = this.$menu.find(".ac-game-menu-field-single");
-        this.$multi = this.$menu.find(". ac-game-menu-field-multi");
-        this.$settings = this.$menu.find(". ac-game-menu-field-settings");
+        this.$single = this.$menu.find(".ac-game-menu-field-single-mode");
+        this.$multi = this.$menu.find(".ac-game-menu-field-multi-mode");
+        this.$settings = this.$menu.find(".ac-game-menu-field-settings");
+
+        this.start();
     }
+
+    start(){
+        this.add_listening_event();
+    }
+
+    add_listening_event(){
+        let outer = this
+        this.$single.click(function (){
+            console.log("click single mod")
+        })
+        this.$multi.click(function (){
+            console.log("click multi mode")
+        })
+        this.$settings.click(function (){
+            console.log("click settings")
+        })
+    }
+
+
 }
