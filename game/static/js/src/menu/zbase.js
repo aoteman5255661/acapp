3dc50cfb6f446e0311df106bc600b69c66f563c7
Your Name <394156907@qq.com>
2022-02-05 16:26:39 +0800

    1

--- game/static/js/src/menu/zbase.js
+++ game/static/js/src/menu/zbase.js
@@ -4,10 +4,25 @@ class AcGameMenu{
         this.root = root;
         this.$menu = $(`
 <div class="ac-game-menu">
+    <div class="ac-game-menu-field">
+        <div class="ac-game-menu-field-item ac-game-menu-field-single" >
+            单人模式
+        </div>
+        <br>
+        <div class="ac-game-menu-field-item ac-game-menu-field-multi">
+            多人模式
+        </div>
+        <br>
+        <div class="ac-game-menu-field-item ac-game-menu-field-item-settings">
+            设置
+        </div>
+    </div>
 </div>
         `);
         console.log(3333)
         this.root.$ac_game.append(this.$menu);
-        console.log(4444)
+        this.$single = this.$menu.find(".ac-game-menu-field-single");
+        this.$multi = this.$menu.find(". ac-game-menu-field-multi");
+        this.$settings = this.$menu.find(". ac-game-menu-field-settings");
     }
 }
