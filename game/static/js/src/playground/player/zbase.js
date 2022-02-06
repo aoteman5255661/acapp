ccc27cd4afec536e0545180c33ddde1a248b3363
Your Name <394156907@qq.com>
2022-02-06 14:04:03 +0800

    1

--- game/static/js/src/playground/player/zbase.js
+++ game/static/js/src/playground/player/zbase.js
@@ -17,6 +17,7 @@ class Player extends AcGameObject{
         this.is_me = is_me;
         this.eps = 0.1;
         this.friction = 0.9;
+        this.spent_time = 0;
 
         this.cur_skill = null;
     }
@@ -82,6 +83,19 @@ class Player extends AcGameObject{
     }
 
     is_attacked(angle, damage){
+
+        for (let i = 0; i < 20 + Math.random()*5; i++){
+            let x = this.x, y = this.y;
+            let radius = this.radius * Math.random() * 0.1;
+            let angle = Math.PI * 2 * Math.random();
+            let vx = Math.cos(angle), vy = Math.sin(angle);
+            let color = this.color;
+            let speed = this.speed * 10;
+            let move_length = this.radius * Math.random() * 10;
+            new Particle(this.playground, x, y, radius, vx, vy, color, speed, move_length);
+        }
+
+
         this.radius -= damage;
         if(this.radius < 10){
             this.destroy();
@@ -90,9 +104,20 @@ class Player extends AcGameObject{
         this.damage_x = Math.cos(angle);
         this.damage_y = Math.sin(angle);
         this.damage_speed = damage * 100;
+        this.speed *= 0.8;
+
     }
 
     update(){
+        this.spent_time += this.timedelta / 1000;
+
+        if(!this.is_me && this.spent_time > 5 && Math.random() < 1 / 300.0){
+            let player = this.playground.players[Math.floor(Math.random() * this.playground.players.length)];
+            let tx = player.x + player.speed * this.vx * this.timedelta / 1000 * 0.3;
+            let ty = player.y + player.speed * this.vy * this.timedelta / 1000 * 0.3;
+            this.shoot_fireball(tx, ty);
+        }
+
         if(this.damage_speed > this.eps){
             this.vx = this.vy = 0;
             this.move_length = 0;
