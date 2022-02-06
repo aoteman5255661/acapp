35c2c3bec559f755d6693f2862279a6d48ea3cc5
Your Name <394156907@qq.com>
2022-02-06 16:21:51 +0800

    1

--- game/static/js/src/playground/player/zbase.js
+++ game/static/js/src/playground/player/zbase.js
@@ -153,4 +153,12 @@ class Player extends AcGameObject{
         this.ctx.fillStyle = this.color;
         this.ctx.fill();
     }
+
+    on_destroy() {
+        for(let i = 0; i < this.playground.players.length; i++){
+            if(this.playground.players[i] === this){
+                this.playground.players.splice(i, 1);
+            }
+        }
+    }
 }
